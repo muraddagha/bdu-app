f1fadbbf7a4399bba5c54357c788797c9cb6e8b3
Your Namemuraddagha <you@example.commuradha@code.edu.az>
2021-09-20 11:29:58 +0400

    some little changes added like line icon added sidebar before

--- src/app/modules/home/components/lesson-held/lesson-held.component.ts
+++ src/app/modules/home/components/lesson-held/lesson-held.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-lesson-held',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LessonHeldComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild("svg") svg: ElementRef
+  public overral: any;
+  constructor() {
+    this.overral = 400
+  }
 
   ngOnInit(): void {
+  }
+  ngAfterViewInit() {
+    this.svg.nativeElement.style.strokeDashoffset = this.overral
   }
 
 }
